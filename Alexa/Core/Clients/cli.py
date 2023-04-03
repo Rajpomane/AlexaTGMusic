6ac295b53d60bad9ead33cab7209954905d40a5e
Raj pomane <95400073+Rajpomane@users.noreply.github.com>
2023-04-04 04:31:47 +0530

    Update cli.py

--- Alexa/Core/Clients/cli.py
+++ Alexa/Core/Clients/cli.py
@@ -26,7 +26,6 @@ else:
     ASS_CLI_1 = Client(
         api_id=API_ID,
         api_hash=API_HASH,
-        session_name=STRING1,
         plugins=dict(root="Alexa.Plugins.Multi-Assistant"),
     )
 
